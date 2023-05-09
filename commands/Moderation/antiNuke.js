dd10704e9914829fc5a0dd62e2620fffe89a5467
RayhanCoder123 <114207244+CoderRayhan000@users.noreply.github.com>
2023-05-09 16:17:56 +1200

    Add files via upload

---
+++ commands/Moderation/antiNuke.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const antiNuke = require("../../models/Antinuke");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("setup-antinuke")
+    .setDescription("Setup AntiNuke for your server"),
+
+  async execute(interaction, client) {
+
+    
+    const guildId = interaction.guildId;
+    const enabled = interaction.options.getBoolean("enabled");
+
+    // const { guild, user, options } = interaction; nothing is being is used
+
+    let antiNukers = await antiNuke.findOne({ guildId: guildId });
+    if (!antiNukers) {
+      antiNukers = new antiNuke({
+        guildId: guildId,
+        enabled: true,
+      });
+      antiNukers.save();
+      interaction.reply({ content: `Setup the anti nuke system`})
+
+    } else {
+      antiNukers.enabled = false;
+      antiNukers.save();
+      interaction.reply({ content: `Found data so deleted the system `})
+    }
+   
+
+  }
+}
