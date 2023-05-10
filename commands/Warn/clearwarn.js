4b03cc93f183213732ef5ecc0cb29e8847ea6a50
RayhanCoder123 <114207244+CoderRayhan000@users.noreply.github.com>
2023-05-10 16:39:45 +1200

    Update clearwarn.js

--- commands/Warn/clearwarn.js
+++ commands/Warn/clearwarn.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionBitField } = require('discord.js')
-const Warn = require('../../models/Warn')
+const Warn = require('../../Ray/models/Warn')
 
 module.exports = {
     data: new SlashCommandBuilder()
