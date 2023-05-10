300de45e2ebfdd6543ea969b779bd054c064b129
RayhanCoder123 <114207244+CoderRayhan000@users.noreply.github.com>
2023-05-10 16:33:56 +1200

    Update mining.js

--- commands/Economy/mining.js
+++ commands/Economy/mining.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder,EmbedBuilder } = require("discord.js")
-const accountSchema = require('../../models/account')
+const accountSchema = require('../../Ray/models/account')
 
 
 module.exports = {
