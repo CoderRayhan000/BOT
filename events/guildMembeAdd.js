3ae265e63e9bd82524051792bf94aa3ab13c1920
RayhanCoder123 <114207244+CoderRayhan000@users.noreply.github.com>
2023-05-10 16:20:55 +1200

    Update guildMembeAdd.js

--- events/guildMembeAdd.js
+++ events/guildMembeAdd.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder, GuildMember, AttachmentBuilder } = require("discord.js");
-const Schema = require('../../models/Join');
+const Schema = require('../../Ray/models/Join');
 const Canvas = require(`canvas`);
 const { readFile } = require('fs/promises');
 const { request } = require('undici');
