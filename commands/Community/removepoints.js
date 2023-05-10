d01d88e414ebb7cc1597649fa468cc48d9727836
RayhanCoder123 <114207244+CoderRayhan000@users.noreply.github.com>
2023-05-10 16:27:23 +1200

    Update removepoints.js

--- commands/Community/removepoints.js
+++ commands/Community/removepoints.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
-const pointSchema = require('../../models/Point')
+const pointSchema = require('../../Ray/models/Point')
 
 
 module.exports = {
